0e3e7f0b762c64186a93af7f0cffe71184ab5344
Gabriel Lemache <lemache35@gmail.com>
2021-11-18 22:45:23 -0300

    ativos

--- components/home/tools/ativos.js
+++ components/home/tools/ativos.js
@@ -1,10 +1,24 @@
 import styles from "../styles/tools/ativos.module.css"
 import {FaSearch, FaStar} from 'react-icons/fa'
-import { useState } from "react"
+import {MdPlayArrow} from 'react-icons/md'
+import { useState, useEffect } from "react"
 import { queryMed } from "../../../lib/api/storeMed"
 
 export default function Ativos(){
     const [wordEntered, setWordEntered] = useState('')
+    const [detail, setDetail] = useState(false)
+    const [data, setData] = useState([])
+    
+    useEffect(()=>{
+        const op = localStorage.getItem('search')
+        const da = localStorage.getItem('data')
+        if(op) setWordEntered(op)
+        if(da) setData(JSON.parse(da))
+      },[])
+      useEffect(()=>{
+        localStorage.setItem('search', wordEntered)
+        localStorage.setItem('data', JSON.stringify(data))
+      })
 
     const handleChange = (e) => {
         setWordEntered(e.target.value)
@@ -12,7 +26,8 @@ export default function Ativos(){
     const handleSubmit = async (e) => {
         e.preventDefault()
         if(wordEntered){
-            await queryMed(wordEntered)
+            const DATA = await queryMed(wordEntered)
+            setData(DATA)
         }
         
     }
@@ -32,18 +47,25 @@ export default function Ativos(){
                 </div>
             </div>
             
-            <div className={styles.tableContainer}>
-                <ul>
-                    <li className={styles.list}>
-                        <div className={styles.listStar}>
-                            <FaStar/>
-                        </div>
-                        <div className={styles.listName}>
-                            <a>Aspirina</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+            {data.slice(1,6).map((value, key)=>{
+                return(
+                    <div className={styles.tableContainer}>
+                    <ul>
+                        <li className={styles.list} key={key}>
+                            <div className={styles.listStar}>
+                                <FaStar/>
+                            </div>
+                            <div className={styles.listName}>
+                                <a>{value['name']}</a>
+                            </div>  
+                            <div onClick={()=>setDetail(!detail)} className={styles.listArrow} >
+                                <MdPlayArrow />
+                            </div>
+                        </li>
+                    </ul>
+                    </div>
+                )
+            })}
         </div>
     )
 }
