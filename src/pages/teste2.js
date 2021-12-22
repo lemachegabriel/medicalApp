3e0d12dbff1c7bc3d513e197c9582b8eccef84f2
Gabriel Lemache <lemache35@gmail.com>
2021-12-22 15:40:28 -0300

    modified:   src/pages/styles/teste.module.css 	modified:   src/pages/teste2.js

--- src/pages/teste2.js
+++ src/pages/teste2.js
@@ -3,8 +3,7 @@ export default function teste2(){
     
 
     return(
-        <>
-            
+        <div style={{backgroundColor: '#fbfbfb', height: '100%', width : '100%', position: 'absolute', zIndex: '-1'}}> 
             <header>
                 <div className={styles.bottomHalf}>
                     <img className={styles.logo} src='/img/logo.svg'/>
@@ -22,18 +21,19 @@ export default function teste2(){
                         <div className={styles.register}>
                             <a>Faça parte da Activus</a>
                         </div>
-                        {/* <div className={styles.containerRest}>
+                        <div className={styles.containerRest}>
                             <div className={styles.descriptions}>
                                 <div className={styles.about}>
-                                    <a>Sua plataforma interativa pronta para sua prescrição magistral</a>
+                                    <h1>Sua plataforma interativa pronta para sua prescrição magistral</h1>
+                                    <a>Tenha em suas mãos dicas de ativos farmaceuticos, posologias, formulações e interações medicamentosas</a>
                                 </div>
                                 
                                 
                             </div>
-                        </div> */}
+                        </div>
                     </div>
                     
                 </div>
                 
-            </>    
+            </div>    
 )}
