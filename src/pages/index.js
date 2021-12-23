a4e63ebdadacba7a4003283e21fe1b2c409c9133
Gabriel Lemache <lemache35@gmail.com>
2021-12-23 09:54:55 -0300

    modified:   src/pages/index.js 	new file:   src/pages/login.js 	modified:   src/pages/styles/index.module.css 	deleted:    src/pages/styles/teste.module.css 	deleted:    src/pages/teste.js 	deleted:    src/pages/teste2.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
-import styles from './styles/home.module.css'
-import Menu from '../components/index/navBar'
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Row, Col } from 'react-bootstrap';
-import Register from '../components/index/RegisterForms';
+import styles from './styles/index.module.css'
 import React, { useState } from 'react';
 import {verify_user_logIn} from '../lib/api/auth'
 import router from 'next/router'
 
+import {BsFileEarmarkText} from 'react-icons/bs'
+import {MdOutlineMedicalServices} from 'react-icons/md'
+import {FaEnvira, FaUserCircle} from 'react-icons/fa'
 
 export default function index(){
     const [register, setRegister] = useState(false);
@@ -17,27 +16,53 @@ export default function index(){
     }
     validate()
     return(
-        <>
-        {register && <Register setReg={setRegister} reg={register}></Register>}
-        <Menu></Menu>
-        <img className={styles.back_img} src='/img/background.svg/'></img>
-        <Row>
-            <Col sm={6} className={styles.texts}>
-                <Row >
-                    <h1>
-                        Sua plataforma interativa pronta para sua prescrição magistral
-                    </h1>
-                    <p>
-                        Tenha em suas mãos dicas de ativos farmaceuticos, posologias, formulações e interações medicamentosas
-                    </p>
-                </Row>
-                <Row>
-                    <Col>
-                        <a onClick={()=> {setRegister(true)}} className={styles.regBut}>Registre-se</a>
-                    </Col>
-                </Row>
-            </Col>
-        </Row>
-        </>
-    )
+        <div style={{backgroundColor: '#fbfbfb', height: '100%', width : '100%', position: 'absolute', zIndex: '-1'}}> 
+            <header>
+                <div className={styles.bottomHalf}>
+                    <img className={styles.logo} src='/img/logo.svg'/>
+                    <div className={styles.menuHeader}>
+                        <FaUserCircle/>
+                        <a>Login</a>
+                    </div>
+                </div>
+            </header>
+                <div className={styles.containerVideo}>
+                    <video className={styles.videoBG} poster='img/poster.png' autoPlay muted loop>
+                        <source src='video/Backvideo Adobecreativecloudexpress-1.m4v' type='video/mp4'></source>
+                    </video>
+                    <div className={styles.title}>
+                        <a>A melhor fórmula de prescrição</a>
+                        <div className={styles.register}>
+                            <a>Faça parte da Activus</a>
+                        </div>
+                        <div className={styles.containerRest}>
+                            <div className={styles.descriptions}>
+                                <div className={styles.about}>
+                                    <h1>Sua plataforma interativa pronta para sua prescrição magistral</h1>
+                                    Tenha em suas mãos dicas de ativos farmaceuticos, posologias, formulações e interações medicamentosas
+                                </div>
+                                <div className={styles.products}>
+                                    <div className={styles.productsInfo}>
+                                        <FaEnvira/>
+                                        <h1>Ativos</h1>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                                    </div>
+                                    <div className={styles.productsInfo}>
+                                        <MdOutlineMedicalServices/>
+                                        <h1>Patologias</h1>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                                    </div>
+                                    <div className={styles.productsInfo}>
+                                        <BsFileEarmarkText/>
+                                        <h1>Receitas</h1>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div> 
+                </div> 
+        </div>    
+)
 }
